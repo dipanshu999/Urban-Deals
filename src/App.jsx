082fc6e9fac73bdc2647744c889b697b317ec0d2
Dipanshu Pandey <dps.coders@gmail.com>
2024-06-23 22:09:56 +0530

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className=' h-screen w-full' >
+      <nav className=' h-[70px] backdrop-blur-3xl sticky top-0 z-30 opacity-60 flex justify-between items-center px-10 border-b border-black'>
+        
+        <div className="logo ">
+          <p className='text-4xl'>Logo</p>
+        </div>
+
+        <div className="navLinks text-4xl ">
+          <ul className=' list-none flex gap-10 font-light'>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Blogs</li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className='h-screen '>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum dolor vel rerum, sed blanditiis accusamus eius minima. Qui nobis culpa eum, eligendi temporibus veniam enim assumenda facere at omnis quasi quam odio ad voluptatem accusamus, earum harum soluta optio commodi possimus esse aspernatur itaque, quo perspiciatis! Magni sequi officia repudiandae consequatur enim vero culpa veritatis non consequuntur, similique doloremque, rem beatae doloribus eos. Sequi, nulla provident. Possimus voluptas cumque corporis, cupiditate nulla reiciendis consequuntur esse reprehenderit accusantium excepturi, quod distinctio inventore voluptatem pariatur! Aperiam ad praesentium vitae, ipsam sint, necessitatibus sapiente atque tempore ipsa possimus perferendis, officiis impedit. Autem, omnis.
+      </div>
+
+      <div className='h-screen'>
+
+      </div>
+    </div>
+  )
+}
