d41263bccd71f85d95e33b3c8cf630f9f8cde0ff
Dipanshu Pandey <dps.coders@gmail.com>
2024-09-09 22:52:16 +0530

    About section enhanced

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,10 +25,9 @@ export default function About() {
   };
 
   return (
-    <div className=' min-h-screen mb-8'>
+    <div className=' mb-8 min-h-screen'>
 
       <div className="profile border rounded-xl shadow-xl py-4 mt-8 w-[90vw] mob:w-[75vw] tab:w-[60vw] lap:w-[40vw] mx-auto">
-
         <div className="pic w-24 h-24 tab:w-32 tab:h-32 rounded-full mx-auto overflow-hidden">
           <img className='w-full h-full object-cover' src='../my-pic.png' alt="profile" />
         </div>
