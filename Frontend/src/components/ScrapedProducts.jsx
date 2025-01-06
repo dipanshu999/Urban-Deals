10e64149a432b5b3d8a904444962de0c300047ac
Dipanshu Pandey <dps.coders@gmail.com>
2025-01-06 14:42:06 +0530

    Backend url added

--- Frontend/src/components/ScrapedProducts.jsx
+++ Frontend/src/components/ScrapedProducts.jsx
@@ -1,29 +1,29 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+  import React, { useEffect } from 'react'
+  import { useParams } from 'react-router-dom'
+  import axios from 'axios'
+  const backendUrl=import.meta.env.VITE_BACKEND_URL
 
-export default function ScrapedProducts() {
-
-    const query=useParams();
-    const category=query.category
-    useEffect(()=>{
-        FetchScrapedProducts()
-        console.log(category)
-       },[])
-    
-      async function FetchScrapedProducts(){
-        const data = await axios.get('http://localhost:3001/api/hello')
-        .then(res=>console.log(res.data))
-        .catch(err=>console.log(err))
-    
-        const pro= await axios.get(`http://localhost:3001/api/scrape?category=${category}`)
-        .then(res=>console.log(res.data))
-        .catch(err=>console.log(err))
-      }
-
-  return (
-    <div>
+  export default function ScrapedProducts() {
+      const query=useParams();
+      const category=query.category
+      useEffect(()=>{
+          FetchScrapedProducts()
+          console.log(category)
+        },[])
       
-    </div>
-  )
-}
+        async function FetchScrapedProducts(){
+          const data = await axios.get(`${backendUrl}/api/hello`)
+          .then(res=>console.log(res.data))
+          .catch(err=>console.log(err))
+      
+          const pro= await axios.get(`${backendUrl}/api/scrape?category=${category}`)
+          .then(res=>console.log(res.data))
+          .catch(err=>console.log(err))
+        }
+
+    return (
+      <div>
+        
+      </div>
+    )
+  }
